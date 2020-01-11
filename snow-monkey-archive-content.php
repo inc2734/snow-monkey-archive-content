2d03851935c90c4bac6e5351d87d0bd4064f248a
Takashi Kitajima <inc@2inc.org>
2020-01-11 10:16:52 +0900

    0.7.0

--- snow-monkey-archive-content.php
+++ snow-monkey-archive-content.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey Archive Content
  * Description: Require Snow Monkey v7.9 or more
- * Version: 0.6.3
+ * Version: 0.7.0
  * Author: inc2734
  * Author URI: https://2inc.org
  * License: GPL2 or later
