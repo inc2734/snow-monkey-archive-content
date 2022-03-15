1310c0850f27b0d37ff5ead43d077ea50e04e276
inc2734 <inc@2inc.org>
2022-03-15 11:51:07 +0900

    1.0.6

--- snow-monkey-archive-content.php
+++ snow-monkey-archive-content.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey Archive Content
  * Description: Activating this plug-in, you will be able to assign pages to archive pages.
- * Version: 1.0.5
+ * Version: 1.0.6
  * Tested up to: 5.9
  * Requires at least: 5.6
  * Requires PHP: 5.6
