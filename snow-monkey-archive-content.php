cc901e03866eea0b81ef179148e52f0d8e74f91e
inc2734 <inc@2inc.org>
2024-11-25 14:47:45 +0900

    Updater を GitHub から独自サーバーに変更

--- snow-monkey-archive-content.php
+++ snow-monkey-archive-content.php
@@ -39,7 +39,8 @@ class Bootstrap {
 	 */
 	public function _plugins_loaded() {
 		add_action( 'init', array( $this, '_load_textdomain' ) );
-		add_action( 'init', array( $this, '_activate_autoupdate' ) );
+
+		new App\Updater();
 
 		$theme = wp_get_theme( get_template() );
 		if ( 'snow-monkey' !== $theme->template && 'snow-monkey/resources' !== $theme->template ) {
@@ -121,20 +122,6 @@ class Bootstrap {
 	 */
 	public function _load_textdomain() {
 		load_plugin_textdomain( 'snow-monkey-archive-content', false, basename( __DIR__ ) . '/languages' );
-	}
-
-	/**
-	 * Activate auto update using GitHub.
-	 */
-	public function _activate_autoupdate() {
-		new \Inc2734\WP_GitHub_Plugin_Updater\Bootstrap(
-			plugin_basename( __FILE__ ),
-			'inc2734',
-			'snow-monkey-archive-content',
-			array(
-				'homepage' => 'https://snow-monkey.2inc.org',
-			)
-		);
 	}
 }
 
